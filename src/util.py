a5cde51300358587c7dcd839ebe5c04ed39ef341
milesChild <colechild1@gmail.com>
2024-01-05 08:52:11 -0500

    change orderdiff

--- src/util.py
+++ src/util.py
@@ -23,6 +23,11 @@ def order_diff(bid: int, ask: int, orders: list, best_bid: int, best_offer: int,
     existing_yes = False
     existing_no = False
 
+    if bid >= best_offer:
+        yes_price = int(best_offer-1)
+    if ask <= best_bid:
+        no_price = int(100 - best_bid-1)
+
     # check for stale prices
     for order in orders:
         # yes checks
@@ -40,13 +45,14 @@ def order_diff(bid: int, ask: int, orders: list, best_bid: int, best_offer: int,
     
     # generate orders
     if not existing_yes:
-        if bid >= best_offer:
-            to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'yes', 'yes_price': int(best_offer-1), 'client_order_id': str(uuid.uuid4())})
-        else:
-            to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'yes', 'yes_price': int(bid), 'client_order_id': str(uuid.uuid4())})
+        if bid > 3 and best_bid > 3:  # TODO: Flawed
+            if bid >= best_offer:
+                to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'yes', 'yes_price': yes_price, 'client_order_id': str(uuid.uuid4())})
+            else:
+                to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'yes', 'yes_price': int(bid), 'client_order_id': str(uuid.uuid4())})
     if not existing_no:
         if ask <= best_bid:
-            to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'no', 'no_price': int(100 - best_bid+1), 'client_order_id': str(uuid.uuid4())})
+            to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'no', 'no_price': no_price, 'client_order_id': str(uuid.uuid4())})
         else:
             to_order.append({'action': 'buy', 'type': 'limit', 'ticker': ticker, 'count': trade_qty, 'side': 'no', 'no_price': int(100 - ask), 'client_order_id': str(uuid.uuid4())})
     
